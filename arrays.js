3da53ddb9ac5cae824dadd30679cf59fa535e1b6
Aidan Clemente <aidanclemente@gmail.com>
2017-10-22 18:06:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,6 +30,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1);
+  removeElementFromBeginningOfArray.slice(1);
   return array;
 }
