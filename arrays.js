7f0045fa72b374696d73795ff45c8c7031c8b685
Aidan Clemente <aidanclemente@gmail.com>
2017-10-22 17:32:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,6 +6,6 @@ function addElementToBeginningOfArray (array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray (array, element) {
-  array.unshift(element);
+  array...(element);
   return array;
 }
